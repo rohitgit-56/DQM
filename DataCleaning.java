839ed91f0c48cb12ba2a7cea919ce48404d7aea1
rohitgit-56 <s.rohit5699@gmail.com>
2025-05-25 10:43:16 +0530

    Data cleaning program Completed.also fixed defects #3479

---
+++ DataCleaning.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+ 
+public class DataCleaner {
+ 
+    public static List<String> cleanData(List<String> data, List<Function<String, String>> cleanOps) {
+        List<String> cleanedData = new ArrayList<>();
+        for (String item : data) {
+            String cleanedItem = item;
+            for (Function<String, String> op : cleanOps) {
+                cleanedItem = op.apply(cleanedItem);
+            }
+            cleanedData.add(cleanedItem);
+        }
+        return cleanedData;
+    }
+ 
+    public static void main(String[] args) {
+        List<Function<String, String>> cleanOps = new ArrayList<>();
+        cleanOps.add(String::toLowerCase);
+        cleanOps.add(str -> str.replaceAll(" ", ""));
+ 
+        List<String> data = new ArrayList<>();
+        data.add("  John Doe  ");
+        data.add("Jane Doe");
+        data.add("  Peter Pan");
+ 
+        List<String> cleanedData = cleanData(data, cleanOps);
+        System.out.println(cleanedData);
+    }
+}
